5206b14662dcd51acf6b11a7d1779b195cff5f26
teamoteh <lim1473@gmail.com>
2021-10-12 03:33:20 +0800

    Longest Common Sequence - done up primitive method

---
+++ LCS/LCSAlgo.java
@@ -0,0 +1,39 @@
+package LCS;
+
+import java.util.Arrays;
+
+public class LCSAlgo {
+    public int LCS(char [] x, char [] y, int n, int m){
+        int[][] arrayProb = new int[n][m];
+
+        //intializing array to be 0 at the start
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                arrayProb[i][j] = 0;
+            }
+        }
+
+        for (int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                //if first character of x and y are similar
+                if(x[i-1] == y[i-1]){
+                    System.out.println(x[i]);
+                    arrayProb[i][j] = arrayProb[i-1][j-1] + 1;
+                }
+                //if left LCS length greater than top LCS length
+                if(arrayProb[i-1][j] >= arrayProb[i][j-1]){
+                    arrayProb[i][j] = arrayProb[i-1][j];
+                    System.out.println(Arrays.deepToString(arrayProb));
+                }
+                else{
+                    arrayProb[i][j] = arrayProb[i][j-1];
+                    System.out.println(Arrays.deepToString(arrayProb));
+                }
+            }
+        }
+
+        return arrayProb[n][m];
+
+    }
+
+}
---
+++ LCS/Main.java
@@ -0,0 +1,17 @@
+package LCS;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Please input first string sequence of characters:");
+        Scanner sc = new Scanner(System.in);
+        char[] x = sc.next().toCharArray();
+        System.out.println("Please input second string sequence of characters:");
+        char[] y = sc.next().toCharArray();
+        LCSAlgo lcs = new LCSAlgo();
+        int result = lcs.LCS(x, y, x.length, y.length);
+
+        System.out.println("The Longest Common Subsequence is: " + result);
+    }
+}
