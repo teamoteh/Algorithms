4cef34c6821ea1f0572a4dcec330b0fd2518ab90
teamoteh <lim1473@gmail.com>
2021-10-14 02:18:15 +0800

    Done up with LCSAlgo

--- LCS/LCSAlgo.java
+++ LCS/LCSAlgo.java
@@ -4,30 +4,35 @@ import java.util.Arrays;
 
 public class LCSAlgo {
     public int LCS(char [] x, char [] y, int n, int m){
-        int[][] arrayProb = new int[n][m];
+        int[][] arrayProb = new int[n + 1][m + 1];
 
-        //intializing array to be 0 at the start
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                arrayProb[i][j] = 0;
-            }
-        }
-
-        for (int i = 1; i < n; i++){
-            for(int j = 1; j < m; j++){
+        for (int i = 0; i <= n; i++){
+            for(int j = 0; j <= m; j++){
                 //if first character of x and y are similar
-                if(x[i-1] == y[i-1]){
-                    System.out.println(x[i]);
+               
+                if(i == 0 || j == 0 ){
+                    arrayProb[i][j] = 0;
+                    continue;
+                }
+
+                if(x[i-1] == y[j-1]){
                     arrayProb[i][j] = arrayProb[i-1][j-1] + 1;
+                    for (int[] row : arrayProb)
+                        System.out.println(Arrays.toString(row));
+                    continue;
                 }
                 //if left LCS length greater than top LCS length
                 if(arrayProb[i-1][j] >= arrayProb[i][j-1]){
                     arrayProb[i][j] = arrayProb[i-1][j];
-                    System.out.println(Arrays.deepToString(arrayProb));
+                    for (int[] row : arrayProb)
+                        System.out.println(Arrays.toString(row));
+                    continue;
                 }
                 else{
                     arrayProb[i][j] = arrayProb[i][j-1];
-                    System.out.println(Arrays.deepToString(arrayProb));
+                    for (int[] row : arrayProb)
+                        System.out.println(Arrays.toString(row));
+                    continue;
                 }
             }
         }
